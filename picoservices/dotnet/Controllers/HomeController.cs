5f9f7d17b4759376a09e9300d4aea23cebab350f
Giovanni Emanuele Nocco <giovanni.nocco@gmail.com>
2021-03-20 12:02:31 +0100

    Added order submit

---
+++ picoservices/dotnet/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Genocs.Microservice.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Genocs.Microservice.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+            => _logger = logger;
+
+        [HttpGet]
+        public IActionResult Get()
+            => Ok("Welcome to .NET!!!");
+
+        [HttpGet("ping")]
+        public IActionResult GetPing()
+            => Ok("pong");
+    }
+}
